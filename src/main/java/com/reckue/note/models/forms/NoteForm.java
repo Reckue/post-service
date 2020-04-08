d7706f84ddc111b036359b1a894aa4bc57a8401c
Hardelele <Hardelele@yahoo.com>
2020-04-08 20:46:58 +0300

    Add NoteForm

---
+++ src/main/java/com/reckue/note/models/forms/NoteForm.java
@@ -0,0 +1,13 @@
+package com.reckue.note.models.forms;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class NoteForm {
+
+    private String payload;
+}
