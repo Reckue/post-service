2eeff5396c3ae3bf3f1bd7d7bc774f5815f20de2
Hardelele <Hardelele@yahoo.com>
2020-04-08 07:08:40 +0300

    Add note with payload field only

---
+++ src/main/java/com/reckue/athena/models/Note.java
@@ -0,0 +1,13 @@
+package com.reckue.athena.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Note {
+
+    private String payload;
+}
