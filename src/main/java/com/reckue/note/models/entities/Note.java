be79ed4c1979cdb9e0c64f251979d3359d4fb517
Hardelele <Hardelele@yahoo.com>
2020-04-09 01:00:03 +0300

    Change UUID to String

--- src/main/java/com/reckue/note/models/entities/Note.java
+++ src/main/java/com/reckue/note/models/entities/Note.java
@@ -5,14 +5,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
-import java.util.UUID;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class Note {
 
     @Id
-    private UUID id;
+    private String id;
     private String payload;
 }
