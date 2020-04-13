820bcd624d992cf2a70d0130ec3b89468cd5bf0c
egnaf <egnaf@yahoo.com>
2020-04-13 10:41:46 +0300

    Add custom handlers and error template class

---
+++ src/main/java/com/reckue/note/models/transfers/ErrorTransfer.java
@@ -0,0 +1,21 @@
+package com.reckue.note.models.transfers;
+
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+public class ErrorTransfer {
+    private String message;
+    private Integer status;
+
+    public ErrorTransfer(int status, Map<String, Object> errorAttributes) {
+        this.setStatus(status);
+        this.setMessage((String) errorAttributes.get("message"));
+    }
+
+    public ErrorTransfer(String message, Integer status) {
+        this.message = message;
+        this.status = status;
+    }
+}
