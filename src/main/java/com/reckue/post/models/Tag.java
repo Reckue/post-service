73b6c32361c60f54f29f4a8d77b47711a7debd87
camelya58 <camelya@mail.ru>
2020-05-22 23:29:14 +0300

    Add class Tag

---
+++ src/main/java/com/reckue/post/models/Tag.java
@@ -0,0 +1,13 @@
+package com.reckue.post.models;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+public class Tag {
+    @Id
+    private String id;
+    private String name;
+}
