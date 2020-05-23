10883bb2b05f62877e132d16c310254cd9df864b
Darkus228 <Dlfacheater@i.ua>
2020-05-23 21:32:36 +0300

    added annotation repo

--- src/main/java/com/reckue/post/repositories/TagRepository.java
+++ src/main/java/com/reckue/post/repositories/TagRepository.java
@@ -2,6 +2,8 @@ package com.reckue.post.repositories;
 
 import com.reckue.post.models.Tag;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TagRepository extends MongoRepository<Tag, String> {
 }
