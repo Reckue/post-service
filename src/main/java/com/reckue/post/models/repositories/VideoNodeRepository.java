8b9d6a82adb6c76bdeb0219a108e3f709a77b4f1
smirnova <dddsssmmm555@mail.ru>
2020-05-23 21:15:30 +0300

    Add video node repository

---
+++ src/main/java/com/reckue/post/models/repositories/VideoNodeRepository.java
@@ -0,0 +1,9 @@
+package com.reckue.post.models.repositories;
+
+import com.reckue.post.models.VideoNode;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VideoNodeRepository extends MongoRepository<VideoNode,String> {
+}
