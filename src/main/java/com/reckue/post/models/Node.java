cd5a77597f4ffda0f9a4ea2a85a4b71a130e6c0c
Viktor Grigoriev <14GameOver@mail.ru>
2020-05-23 21:10:14 +0300

    Add class Node

--- src/main/java/com/reckue/post/models/Node.java
+++ src/main/java/com/reckue/post/models/Node.java
@@ -1,4 +1,18 @@
 package com.reckue.post.models;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
 public class Node {
+
+    @Id
+    String id;
+
+    NodeType type;
+    String contentId;
+    String source;
+    StatusType status;
 }
