0109767d33eaf9c1eb2cdc55f9ecc3656f6b139d
camelya58 <camelya@mail.ru>
2020-05-24 23:35:39 +0300

    Add classes request and response for model post

---
+++ src/main/java/com/reckue/post/transfers/PostRequest.java
@@ -0,0 +1,20 @@
+package com.reckue.post.transfers;
+
+import com.reckue.post.models.Node;
+import com.reckue.post.models.StatusType;
+import com.reckue.post.models.Tag;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class PostRequest {
+
+    private String title;
+    private List<Node> nodes;
+    private String source;
+    private List<Tag> tags;
+    private long published;
+    private long changed;
+    private StatusType status;
+}
