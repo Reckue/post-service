2cf1e041470616aa93e4c3b7a767328f1dac4bb4
camelya58 <camelya@mail.ru>
2020-05-25 23:25:52 +0300

    Add node service and node service realization

---
+++ src/main/java/com/reckue/post/services/NodeService.java
@@ -0,0 +1,50 @@
+package com.reckue.post.services;
+
+import com.reckue.post.models.Node;
+
+import java.util.List;
+
+/**
+ * Interface NodeService represents service with CRUD operations.
+ *
+ * @author Kamila Meshcheryakova
+ */
+public interface NodeService {
+
+    /**
+     * This method is used to create an object of class Node.
+     * @param node object of class Node
+     * @return node object of class Node
+     */
+    Node create(Node node);
+
+    /**
+     * This method is used to update data in an object of class Node.
+     * @param node object of class Node
+     * @return node object of class Node
+     */
+    Node update(Node node);
+
+    /**
+     * This method is used to get all objects of class Node by parameters.
+     * @param limit quantity of objects
+     * @param offset quantity to skip
+     * @param sort parameter for sorting
+     * @param desc sorting descending
+     * @return list of objects of class Node
+     */
+    List<Node> findAll(int limit, int offset, String sort, boolean desc);
+
+    /**
+     * This method is used to get an object by id.
+     * @param id object
+     * @return object of class Node
+     */
+    Node findById(String id);
+
+    /**
+     * This method is used to delete an object by id.
+     * @param id object
+     */
+    void deleteById(String id);
+}
