5240d5f66f3938b7580f798825e74d674b9f78d6
IveriNarozashvili <magari16-11@mail.ru>
2020-05-26 22:39:25 +0300

    Add converter

--- src/main/java/com/reckue/post/utils/converters/Converter.java
+++ src/main/java/com/reckue/post/utils/converters/Converter.java
@@ -4,12 +4,23 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.modelmapper.ModelMapper;
 
+/**
+ * Method converts one object to another.
+ */
 @NoArgsConstructor
 @Data
 public class Converter {
 
     private static ModelMapper mapper;
 
+    /**
+     * This method convert
+     *
+     * @param src  object to be converted
+     * @param dist class type of destination object
+     * @param <T>  convertible type
+     * @return converted object
+     */
     public static <T> T convert(Object src, Class<T> dist) {
         return mapper.map(src, dist);
     }
