2af7092057f0ea8051d10651c3bdd05d65dd853f
camelya58 <camelya@mail.ru>
2020-05-27 11:52:57 +0300

    Add configuration of swagger ui

---
+++ src/main/java/com/reckue/post/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.reckue.post.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Class SwaggerConfig set up settings for swagger.
+ *
+ * @author Kamila Meshcheryakova
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * This method is used to set up tittle and description for swagger.
+     *
+     * @return page swagger-ui.html with custom fields
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Post API")
+                .description("Service for posting articles about different programming languages.")
+                .version("version:0.1")
+                .contact(new Contact("Reckue", "www.reckue.com", "support@reckue.com"))
+                .build();
+    }
+}
