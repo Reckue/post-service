7227280f145ae29e1b9a9b56739924163993726f
Viktor Grigoriev <14GameOver@mail.ru>
2020-05-27 21:05:25 +0300

    Add video node service and realization.

--- src/main/java/com/reckue/post/services/VideoNodeService.java
+++ src/main/java/com/reckue/post/services/VideoNodeService.java
@@ -5,15 +5,46 @@ import com.reckue.post.models.VideoNode;
 import java.util.List;
 
 /**
- * CRUD operations for VideoNode class.
+ * Interface VideoNodeService represents service with CRUD operations.
  *
- * @author Viktor
+ * @author Viktor Grigoriev
  */
 public interface VideoNodeService {
 
+    /**
+     * This method is used to create an object of class Node.
+     * @param videoNode object of class VideoNode
+     * @return videoNode object of class VideoNode
+     */
     VideoNode create(VideoNode videoNode);
+
+    /**
+     * This method is used to update data in an object of class VideoNode.
+     * @param videoNode object of class VideoNode
+     * @return videoNode object of class VideoNode
+     */
     VideoNode update(VideoNode videoNode);
-    List findAll(int limit, int offset, String sort, boolean desc);
+
+    /**
+     * This method is used to get all objects of class VideoNode by parameters.
+     * @param limit quantity of objects
+     * @param offset quantity to skip
+     * @param sort parameter for sorting
+     * @param desc sorting descending
+     * @return list of objects of class VideoNode
+     */
+    List<VideoNode> findAll(int limit, int offset, String sort, boolean desc);
+
+    /**
+     * This method is used to get an object by id.
+     * @param id object
+     * @return object of class VideoNode
+     */
     VideoNode findById(String id);
+
+    /**
+     * This method is used to delete an object by id.
+     * @param id object
+     */
     void deleteById(String id);
 }
--- src/main/java/com/reckue/post/services/realizations/VideoNodeServiceRealization.java
+++ src/main/java/com/reckue/post/services/realizations/VideoNodeServiceRealization.java
@@ -1,9 +1,11 @@
 package com.reckue.post.services.realizations;
 
+import com.google.common.collect.Lists;
 import com.reckue.post.exceptions.ModelNotFoundException;
 import com.reckue.post.models.VideoNode;
 import com.reckue.post.repositories.VideoNodeRepository;
 import com.reckue.post.services.VideoNodeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Comparator;
@@ -11,19 +13,41 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+/**
+ * Class VideoServiceRealization represents realization of VideoNodeService.
+ *
+ * @author Viktor Grigoriev
+ */
+@RequiredArgsConstructor
 public class VideoNodeServiceRealization implements VideoNodeService {
 
     @Autowired
     private VideoNodeRepository videoNodeRepository;
 
+    /**
+     * This method is used to create an object of class VideoNode.
+     * Throws {@link ModelNotFoundException} in case if such object not found exists.
+     * @param videoNode object of class VideoNode
+     * @return videoNode object of class VideoNode
+     */
     @Override
-    public VideoNode create (VideoNode textNode) {
+    public VideoNode create (VideoNode videoNode) {
         VideoNode savedTextNode;
-        savedTextNode = textNode;
+        savedTextNode = videoNode;
         savedTextNode.setId(UUID.randomUUID().toString());
-        return videoNodeRepository.save(textNode);
+        return videoNodeRepository.save(videoNode);
     }
 
+    /**
+     * This method is used to update data in an object of class VideoNode.
+     * Throws {@link ModelNotFoundException} in case
+     * if such object isn't contained in database.
+     * Throws {@link IllegalArgumentException} in case
+     * if such parameter is null.
+     *
+     * @param videoNode object of class VideoNode
+     * @return videoNode object of class VideoNode
+     */
     @Override
     public VideoNode update(VideoNode videoNode) {
         VideoNode savedVideoNode;
@@ -36,14 +60,82 @@ public class VideoNodeServiceRealization implements VideoNodeService {
         return videoNodeRepository.save(savedVideoNode);
     }
 
+    /**
+     * This method is used to get all objects of class VideoNode.
+     *
+     * @return list of objects of class VideoNode
+     */
+    public List<VideoNode> findAll() {
+        return videoNodeRepository.findAll();
+    }
+
+    /**
+     * This method is used to get all objects of class VideoNode by parameters.
+     *
+     * @param limit  quantity of objects
+     * @param offset quantity to skip
+     * @param sort   parameter for sorting
+     * @param desc   sorting descending
+     * @return list of objects of class VideoNode
+     */
     @Override
     public List<VideoNode> findAll(int limit, int offset, String sort, boolean desc) {
-        return videoNodeRepository.findAll().stream()
+        return findAllByTypeAndDesc(sort, desc).stream()
                 .limit(limit)
                 .skip(offset)
-                .sorted(Comparator.comparing(VideoNode::getId, Comparator.reverseOrder()))
                 .collect(Collectors.toList());
     }
+
+    /**
+     * This method is used to sort objects in descending order by type.
+     *
+     * @param sort parameter for sorting
+     * @param desc sorting descending
+     * @return list of objects of class VideoNode sorted by the selected parameter for sorting
+     * in descending order
+     */
+    public List<VideoNode> findAllByTypeAndDesc(String sort, boolean desc) {
+        if (desc) {
+            return Lists.reverse(findAllBySortType(sort));
+        }
+        return findAllBySortType(sort);
+    }
+
+    /**
+     * This method is used to sort objects by type.
+     *
+     * @param sort type of sorting: content, default - id
+     * @return list of objects of class VideoNode sorted by the selected parameter for sorting
+     */
+    public List<VideoNode> findAllBySortType(String sort) {
+        if (sort.equals("videoUrl")) {
+            return findAllAndSortByVideoUrl();
+        }
+        return findAllAndSortById();
+    }
+
+    /**
+     * This method is used to sort objects by id.
+     *
+     * @return list of objects of class VideoNode sorted by id
+     */
+    public List<VideoNode> findAllAndSortById() {
+        return findAll().stream()
+                .sorted(Comparator.comparing(VideoNode::getId))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * This method is used to sort objects by videoUrl.
+     *
+     * @return list of objects of class VideoNode sorted by content
+     */
+    public List<VideoNode> findAllAndSortByVideoUrl() {
+        return findAll().stream()
+                .sorted(Comparator.comparing(VideoNode::getVideoUrl))
+                .collect(Collectors.toList());
+    }
+
 
     @Override
     public VideoNode findById(String id) {
