b70b427106de799f1080407c43f9fbb7cdf8c066
IveriNarozashvili <magari16-11@mail.ru>
2020-06-03 23:06:01 +0300

    Add javadoc to the models

--- src/main/java/com/reckue/post/models/CodeNode.java
+++ src/main/java/com/reckue/post/models/CodeNode.java
@@ -5,6 +5,11 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+/**
+ * Class CodeNode represents model of code fields.
+ *
+ * @author Iveri Narozashvili
+ */
 @Data
 @Builder
 @Document
--- src/main/java/com/reckue/post/models/StatusType.java
+++ src/main/java/com/reckue/post/models/StatusType.java
@@ -1,5 +1,10 @@
 package com.reckue.post.models;
 
+/**
+ * This enum StatusType represents enumeration of statuses that are associated with post or node.
+ *
+ * @author Iveri Narozashvili
+ */
 public enum StatusType {
 
     ACTIVE,
