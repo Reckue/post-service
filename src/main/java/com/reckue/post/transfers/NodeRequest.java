7fd666bbaf5cb5cb667fa1cfc39649f1781ed6b3
Viktor Grigoriev <14GameOver@mail.ru>
2020-06-05 22:55:08 +0300

    Add integration test for node controller

--- src/main/java/com/reckue/post/transfers/NodeRequest.java
+++ src/main/java/com/reckue/post/transfers/NodeRequest.java
@@ -2,12 +2,14 @@ package com.reckue.post.transfers;
 
 import com.reckue.post.models.NodeType;
 import com.reckue.post.models.StatusType;
+import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
+@Builder
 public class NodeRequest {
 
     @NotNull
