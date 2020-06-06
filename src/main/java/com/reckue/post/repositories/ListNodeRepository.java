30b9a72b6d1296fecfc74d2ae057064ee74257e8
smirnova <dddsssmmm555@mail.ru>
2020-06-06 21:55:49 +0300

    Delete extra repositories

--- src/main/java/com/reckue/post/repositories/ListNodeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.reckue.post.repositories;
-
-import com.reckue.post.models.ListNode;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ListNodeRepository extends MongoRepository<ListNode, String> {
-}
