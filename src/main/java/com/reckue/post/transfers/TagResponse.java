8c51751f941e4f6caf0d968bba481ca9db3b6fa9
smirnova <dddsssmmm555@mail.ru>
2020-06-08 22:41:44 +0300

    Add test for convertor class

--- src/main/java/com/reckue/post/transfers/TagResponse.java
+++ src/main/java/com/reckue/post/transfers/TagResponse.java
@@ -1,10 +1,16 @@
 package com.reckue.post.transfers;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
 public class TagResponse {
 
     private String id;
