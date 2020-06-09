de06fac6b69dbaf3676c9f8cb0e4e5276f4ce7d5
Sherzod Mamadaliev <egnaf@yahoo.com>
2020-06-09 13:28:04 +0300

    Test auto deploy 6

--- src/main/java/com/reckue/post/controllers/TestController.java
+++ src/main/java/com/reckue/post/controllers/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test4")
+@RequestMapping("/test5")
 public class TestController {
 
     @GetMapping("/")
