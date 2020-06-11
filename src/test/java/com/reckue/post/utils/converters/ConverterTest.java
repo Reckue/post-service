24b284ea32a63a0f66bb95fc606ac8fb14a4f867
camelya58 <camelya@mail.ru>
2020-06-11 17:50:00 +0300

    Add custom classes for testing and changes

--- src/test/java/com/reckue/post/utils/converters/ConverterTest.java
+++ src/test/java/com/reckue/post/utils/converters/ConverterTest.java
@@ -1,8 +1,8 @@
 package com.reckue.post.utils.converters;
 
+import com.reckue.post.utils.Check;
+import com.reckue.post.utils.CheckResponse;
 import com.reckue.post.PostServiceApplicationTests;
-import com.reckue.post.models.Tag;
-import com.reckue.post.transfers.TagResponse;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -20,60 +20,60 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class ConverterTest extends PostServiceApplicationTests {
 
     @Test
-    void convertTagToTagResponse() {
-        Tag tag = Tag.builder()
+    void convertCheckToCheckResponse() {
+        Check check = Check.builder()
                 .id("1")
-                .name("sherzod")
+                .name("egnaf")
                 .build();
 
-        TagResponse expected = TagResponse.builder().
-                id(tag.getId())
-                .name(tag.getName())
+        CheckResponse expected = CheckResponse.builder()
+                .id(check.getId())
+                .name(check.getName())
                 .build();
 
-        TagResponse actual = Converter.convert(tag, TagResponse.class);
+        CheckResponse actual = Converter.convert(check, CheckResponse.class);
         assertEquals(expected, actual);
     }
 
     @Test
-    void checkTagIsNull() {
+    void checkCheckIsNull() {
         Throwable exception = assertThrows(IllegalArgumentException.class,
-                () -> Converter.convert((Object) null, TagResponse.class));
+                () -> Converter.convert((Object) null, CheckResponse.class));
         assertEquals("Null parameters are not allowed", exception.getMessage());
     }
 
     @Test
     void convertListToAnotherList() {
-        Tag tag1 = Tag.builder()
+        Check check1 = Check.builder()
                 .id("1")
                 .name("daria")
                 .build();
-        Tag tag2 = Tag.builder()
+        Check check2 = Check.builder()
                 .id("2")
                 .name("camelya")
                 .build();
-        List<Tag> tags = Stream.of(tag1,tag2).collect(Collectors.toList());
+        List<Check> checks = Stream.of(check1,check2).collect(Collectors.toList());
 
-        TagResponse tagResponse1 = TagResponse.builder()
-                .id(tag1.getId())
-                .name(tag1.getName())
+        CheckResponse checkResponse1 = CheckResponse.builder()
+                .id(check1.getId())
+                .name(check1.getName())
                 .build();
-        TagResponse tagResponse2 = TagResponse.builder()
-                .id(tag2.getId())
-                .name(tag2.getName())
+        CheckResponse checkResponse2 = CheckResponse.builder()
+                .id(check2.getId())
+                .name(check2.getName())
                 .build();
 
-        List<TagResponse> expected = Stream.of(tagResponse1, tagResponse2).collect(Collectors.toList());
+        List<CheckResponse> expected = Stream.of(checkResponse1, checkResponse2).collect(Collectors.toList());
 
-        List<TagResponse> actual = Converter.convert(tags, TagResponse.class);
+        List<CheckResponse> actual = Converter.convert(checks, CheckResponse.class);
         assertEquals(expected, actual);
     }
 
     @Test
     void checkListIsNull() {
-        List<Tag> tags = null;
+        List<Check> checks = null;
         Throwable exception = assertThrows(IllegalArgumentException.class,
-                () -> Converter.convert(tags, TagResponse.class));
+                () -> Converter.convert(checks, CheckResponse.class));
         assertEquals("Null parameters are not allowed", exception.getMessage());
     }
 }
