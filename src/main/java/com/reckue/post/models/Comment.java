f4f4881371b775a3a4c5fcb7fc7c94e4389728a0
Artur <coungard@mail.ru>
2020-06-17 17:51:02 +0300

    Add a comment model

---
+++ src/main/java/com/reckue/post/models/Comment.java
@@ -0,0 +1,29 @@
+package com.reckue.post.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+/**
+ * Class Comment is responsible for model that works with comments.
+ *
+ * @author Artur Magomedov
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
+public class Comment {
+
+    @Id
+    private String id;
+
+    private String userId;
+    private List<Node> nodeList;
+}
