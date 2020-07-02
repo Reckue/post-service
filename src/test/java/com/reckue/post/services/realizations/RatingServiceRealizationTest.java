a8f16604a69591533f60af9c4678f796c0581581
camelya58 <camelya@mail.ru>
2020-07-02 23:58:05 +0300

    Add changes to rating service realization test

--- src/test/java/com/reckue/post/services/realizations/RatingServiceRealizationTest.java
+++ src/test/java/com/reckue/post/services/realizations/RatingServiceRealizationTest.java
@@ -1,9 +1,13 @@
 package com.reckue.post.services.realizations;
 
+import com.reckue.post.PostServiceApplicationTests;
 import com.reckue.post.exceptions.ModelAlreadyExistsException;
 import com.reckue.post.exceptions.ModelNotFoundException;
+import com.reckue.post.models.Post;
 import com.reckue.post.models.Rating;
+import com.reckue.post.repositories.PostRepository;
 import com.reckue.post.repositories.RatingRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -24,58 +28,100 @@ import static org.mockito.Mockito.*;
  *
  * @author Iveri Narozashvili
  */
-public class RatingServiceRealizationTest extends PostServiceRealizationTest {
+@SuppressWarnings("unused")
+public class RatingServiceRealizationTest extends PostServiceApplicationTests {
+    private Rating rating1;
+    private Rating rating2;
+    private Rating rating3;
+    private Post post;
+
     @Mock
     private RatingRepository ratingRepository;
+
+    @Mock
+    private PostRepository postRepository;
 
     @InjectMocks
     private RatingServiceRealization ratingService;
 
+    @BeforeEach
+    private void createNeeded() {
+        rating1 = Rating.builder()
+                .id("1")
+                .userId("1a35")
+                .postId("1ft2")
+                .build();
+        rating2 = Rating.builder()
+                .id("2")
+                .userId("2da3")
+                .postId("3dt5")
+                .build();
+        rating3 = Rating.builder()
+                .id("3")
+                .userId("7gd3")
+                .postId("8uv2")
+                .build();
+    }
+
     @Test
     public void findAll() {
-        Rating rating1 = Rating.builder()
-                .id("1")
-                .build();
-        Rating rating2 = Rating.builder()
-                .id("2")
-                .build();
-        List<Rating> nodes = Stream.of(rating1, rating2).collect(Collectors.toList());
+        List<Rating> nodes = Stream.of(rating1, rating2, rating3).collect(Collectors.toList());
         when(ratingRepository.findAll()).thenReturn(nodes);
 
         assertEquals(nodes, ratingService.findAll());
     }
 
+    /*
     @Test
     public void create() {
+        post = Post.builder()
+                .id("2rs5")
+                .build();
+        postRepository.save(post);
         Rating rating = Rating.builder()
-                .id("1")
+                .id("4")
+                .userId("5tf4")
+                .postId(post.getId())
                 .build();
         when(ratingRepository.save(rating)).thenReturn(rating);
 
         assertEquals(rating, ratingService.create(rating));
     }
+     */
 
     @Test
     public void createIfExists() {
-        Rating rating = Rating.builder()
-                .id("1")
-                .build();
         doReturn(true).when(ratingRepository).existsById(Mockito.anyString());
-        Exception exception = assertThrows(ModelAlreadyExistsException.class, () -> ratingService.create(rating));
+        Exception exception = assertThrows(ModelAlreadyExistsException.class, () -> ratingService.create(rating1));
 
         assertEquals("Rating already exists", exception.getMessage());
     }
 
     @Test
+    public void createIfNotFound() {
+        when(ratingRepository.existsById(rating1.getId())).thenReturn(false);
+        Exception exception = assertThrows(ModelNotFoundException.class, () -> ratingService.create(rating1));
+
+        assertEquals("Post identifier '" + rating1.getPostId() + "' is not found", exception.getMessage());
+    }
+    /*
+    @Test
     public void update() {
+        post = Post.builder()
+                .id("2rs5")
+                .build();
+        postRepository.save(post);
         Rating ratingOne = Rating.builder()
-                .id("1")
+                .id(rating1.getId())
+                .userId(rating1.getUserId())
+                .postId(rating1.getPostId())
                 .build();
         when(ratingRepository.existsById(ratingOne.getId())).thenReturn(true);
         when(ratingRepository.save(ratingOne)).thenReturn(ratingOne);
 
         assertEquals(ratingOne, ratingService.update(ratingOne));
     }
+     */
 
     @Test
     public void updateWithNullId() {
@@ -87,59 +133,44 @@ public class RatingServiceRealizationTest extends PostServiceRealizationTest {
 
     @Test
     public void updateWithExistId() {
-        Rating ratingOne = Rating.builder()
-                .id("1")
-                .build();
-        when(ratingRepository.existsById(ratingOne.getId())).thenReturn(false);
-        when(ratingRepository.save(ratingOne)).thenReturn(ratingOne);
+        when(ratingRepository.existsById(rating1.getId())).thenReturn(false);
+        when(ratingRepository.save(rating1)).thenReturn(rating1);
 
-        assertThrows(ModelNotFoundException.class, () -> ratingService.update(ratingOne));
+        assertThrows(ModelNotFoundException.class, () -> ratingService.update(rating1));
     }
 
     @Test
     public void findByIdWithException() {
-        Rating ratingOne = Rating.builder()
-                .id("1")
-                .build();
-        when(ratingRepository.findById(ratingOne.getId())).thenReturn(Optional.empty());
+        when(ratingRepository.findById(rating1.getId())).thenReturn(Optional.empty());
 
-        assertThrows(ModelNotFoundException.class, () -> ratingService.findById(ratingOne.getId()));
+        assertThrows(ModelNotFoundException.class, () -> ratingService.findById(rating1.getId()));
     }
 
     @Test
     public void findById() {
-        Rating ratingOne = Rating.builder()
-                .id("1")
-                .build();
-        when(ratingRepository.findById(ratingOne.getId())).thenReturn(Optional.of(ratingOne));
+        when(ratingRepository.findById(rating1.getId())).thenReturn(Optional.of(rating1));
 
-        assertEquals(ratingOne, ratingService.findById(ratingOne.getId()));
+        assertEquals(rating1, ratingService.findById(rating1.getId()));
     }
 
     @Test
     public void deleteById() {
-        Rating ratingOne = Rating.builder()
-                .id("1")
-                .build();
-        List<Rating> rating = new ArrayList<>();
-        rating.add(ratingOne);
-        when(ratingRepository.existsById(ratingOne.getId())).thenReturn(true);
+        List<Rating> ratings = new ArrayList<>();
+        ratings.add(rating1);
+        when(ratingRepository.existsById(rating1.getId())).thenReturn(true);
         doAnswer(invocation -> {
-            rating.remove(ratingOne);
+            ratings.remove(rating1);
             return null;
-        }).when(ratingRepository).deleteById(ratingOne.getId());
-        ratingService.deleteById(ratingOne.getId());
+        }).when(ratingRepository).deleteById(rating1.getId());
+        ratingService.deleteById(rating1.getId());
 
-        assertEquals(0, rating.size());
+        assertEquals(0, ratings.size());
     }
 
     @Test
     public void deleteByIdWithException() {
-        Rating ratingOne = Rating.builder()
-                .id("0")
-                .build();
         Exception exception = assertThrows(ModelNotFoundException.class,
-                () -> ratingService.deleteById(ratingOne.getId()));
-        assertEquals("Rating by id " + ratingOne.getId() + " is not found", exception.getMessage());
+                () -> ratingService.deleteById(rating1.getId()));
+        assertEquals("Rating by id " + rating1.getId() + " is not found", exception.getMessage());
     }
 }
