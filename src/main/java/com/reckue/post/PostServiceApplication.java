f5b8580269a19d0387523f4d5979705373e6d663
egnaf <egnaf@yahoo.com>
2020-09-19 22:53:49 +0300

    Add open api config and yaml file

--- src/main/java/com/reckue/post/PostServiceApplication.java
+++ src/main/java/com/reckue/post/PostServiceApplication.java
@@ -25,6 +25,7 @@
  */
 package com.reckue.post;
 
+import com.reckue.post.generated.models.InternalServerError;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -46,6 +47,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class PostServiceApplication {
 
     public static void main(String[] args) {
+        InternalServerError e = new InternalServerError();
         SpringApplication.run(PostServiceApplication.class, args);
     }
 }
