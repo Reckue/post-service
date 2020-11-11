2d80c63255a9badf9153b0f55f28be2883b2bb04
Sherzod Mamadaliev <egnaf@yahoo.com>
2020-11-11 17:39:56 +0300

    Fix license extra scape

--- src/main/java/com/reckue/post/PostServiceApplication.java
+++ src/main/java/com/reckue/post/PostServiceApplication.java
@@ -22,7 +22,7 @@
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
   SOFTWARE.
 
- */
+*/
 package com.reckue.post;
 
 import org.springframework.boot.SpringApplication;
