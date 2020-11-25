6d65e57aa8ae46e4c19ec11b618c8b0e538e2b14
Hardelele <Hardelele@yahoo.com>
2020-11-26 01:09:32 +0300

    Add Sandbox class and gradle run script

---
+++ src/test/java/com/reckue/post/sandbox/Sandbox.java
@@ -0,0 +1,13 @@
+package com.reckue.post.sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Sandbox {
+
+    @Test
+    public void run() {
+
+    }
+}
